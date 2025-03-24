4e1f8ef078ed0127a53c1a14e14db5b7405d4dd7
vy <vyshooter@gmail.com>
2025-03-24 21:07:43 +0800

    RUN!

--- seg/configs/_base_/schedules/schedule_60k.py
+++ seg/configs/_base_/schedules/schedule_60k.py
@@ -5,5 +5,5 @@ optimizer_config = dict()
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
 runner = dict(type='IterBasedRunner', max_iters=30000)
-#checkpoint_config = dict(by_epoch=False, interval=10000, max_keep_ckpts=1)
+checkpoint_config = None
 evaluation = dict(interval=3000, metric='mIoU', save_best='mIoU')
